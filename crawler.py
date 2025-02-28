ff75b0a7dcd12a52d717ab2d27421be34887d986
sahiljagad <sahiljagad@gmail.com>
2025-02-28 11:14:55 -0800

    gets all event urls for events within a week from current date

--- crawler.py
+++ crawler.py
@@ -1,29 +1,32 @@
 from random import uniform
 import requests
 import time
-import csv
+import re
 from datetime import date, timedelta
 from bs4 import BeautifulSoup
+
+#function to create starting url list
+def populate_url_start_set(today, urls):
+    for i in range(1,8):
+        day = today + timedelta(days=i)
+        urls.add(f"https://dothebay.com/events/{day.year}/{day.month}/{day.day}")
+
 
 # Get the current date
 today = date.today()
 week_from_now = today + timedelta(days=7)
 
-# Extract day, month, and year
-current_day = today.day
-current_month = today.month
-current_year = today.year
+# Extract day, month, and year into lists
+today_list = [today.year, today.month, today.day]
+future_list = [week_from_now.year, week_from_now.month, week_from_now.day]
 
-future_day = week_from_now.day
-future_month = week_from_now.month
-future_year = week_from_now.year
+#url regex
+regxp = re.compile(r"((\/events\/)|(\/\?page=\d))((\d{4})\/(\d{0,2})\/(\d{0,2}))")
     
-# initialize the list of discovered urls
-# with the first page to visit
-# start_url = f"https://visitseattle.org/?s=&frm=events&event_begin={current_month}%2F{current_day}%2F{current_year}&event_end={future_month}%2F{future_day}%2F{future_year}&partner_parent_cat="
+# initialize the list of discovered urls with the pages of events in next
 
-start_url = "https://visitseattle.org/events/law-rocks/"
-urls = { start_url }
+urls = set()
+populate_url_start_set(today, urls)
 visited = []
 data = {}
 
@@ -44,27 +47,21 @@ while len(urls) != 0:
         # Add a random delay between 3 and 7 seconds
         time.sleep(uniform(3, 7))
         soup = BeautifulSoup(response.content, "html.parser")
-            
 
         for link_element in soup.find_all('a', href=True):
             url = link_element['href']
-            if ("https://visitseattle.org" in url) and ("frm=events" in url or "https://visitseattle.org/events/" in url):
-                urls.add(url)
-            
-        if "frm=events" not in current_url:
-            event = {}
-            event["name"] = soup.select_one('.event-top .page-title').text
-            event["date"] = soup.select('.event-top h4 span')[0].text
-            event['location'] = soup.select('.event-top h4 span')[1].text
-            event['category'] = list(map(lambda x: x.text, soup.select('.category')))
-            event["description"] = soup.select('.container-event-detail p')[1].text
-            event["website"] = soup.select('.container-event-detail .medium-6  li a')[-1].text
+            if (regxp.match(url)) is not None: 
+                if 'page=' in url:
+                    urls.add("https://dothebay.com" + url)
+                else:
+                    try:
+                        url_date = map(lambda x:int(x), url.split("/")[2:5])
+                        if today_list <= list(url_date) <= future_list:
+                            urls.add("https://dothebay.com" + url)
+                    except:
+                        print(url, "not visited.")
+            if len(visited) == 100:
+                break
 
-        print(event)
-
-        break
-
-        print("Visited: ", visited, "\n")
-        print("Urls to visit: ", urls, "\n")
-
+print(len(visited), visited)
 print("Done")
